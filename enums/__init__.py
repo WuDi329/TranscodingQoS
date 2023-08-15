ea41f11d246156a334222d301ebb8a7fb5fbf0ed
wudi-ffmpeg <wuuudiii@qq.com>
2023-08-15 14:36:36 +0000

    refactor: 杂

--- enums/__init__.py
+++ enums/__init__.py
@@ -2,8 +2,9 @@ from .resolution import Resolution
 from .video_codec import VideoCodec
 from .bitrate import Bitrate
 from .audio_codec import AudioCodec
+from .accelerator import Accelerator
 
 
 
 
-__all__ = ["Resolution", "VideoCodec", "Bitrate", "AudioCodec"]
+__all__ = ["Resolution", "VideoCodec", "Bitrate", "AudioCodec", "Accelerator"]
