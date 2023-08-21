a9cee8df2821627a5c4e3d31d83c4c64a8fa0bad
wudi-ffmpeg <wuuudiii@qq.com>
2023-08-21 11:48:30 +0000

    feat: 修改device代码，设计device类

--- transcode/device.py
+++ transcode/device.py
@@ -3,36 +3,42 @@ import socket
 import fcntl
 import struct
 
-def get_mac_address():
-    """
-        获取设备的MAC地址.
+class Node:
+    def __init__(self, nodeid, ip, port):
+        self._macaddress = self.get_device_uuid()
 
-        Returns:
-            mac_address (str): 设备的MAC地址.
-    """
-    # 获取设备的名称
-    ifname = "eth0"
+    @property
+    def macaddress(self):
+        return self._macaddress
 
-    # 获取设备的MAC地址
-    with open('/sys/class/net/%s/address' % ifname) as f:
-        mac_address = f.read().strip()
+    def get_mac_address(self):
+        """
+            获取设备的MAC地址.
 
-    return mac_address
+            Returns:
+                mac_address (str): 设备的MAC地址.
+        """
+        # 获取设备的名称
+        ifname = "eth0"
 
-def get_device_uuid():
-    """
-        获取设备的唯一标识符.
+        # 获取设备的MAC地址
+        with open('/sys/class/net/%s/address' % ifname) as f:
+            mac_address = f.read().strip()
 
-        Returns:
-            device_uuid (str): 设备的唯一标识符.
-    """
-    # 获取设备的MAC地址
-    mac_address = get_mac_address()
+        return mac_address
 
-    # 将MAC地址转换为UUID
-    device_uuid = mac_address.replace(':', '').upper()
+    def get_device_uuid(self):
+        """
+            获取设备的唯一标识符.
 
-    return device_uuid
+            Returns:
+                device_uuid (str): 设备的唯一标识符.
+        """
+        # 获取设备的MAC地址
+        mac_address = self.get_mac_address()
 
-device_uuid = get_device_uuid()
-print(device_uuid)
+        # 将MAC地址转换为UUID
+        device_uuid = mac_address.replace(':', '').upper()
+
+        return device_uuid
+
